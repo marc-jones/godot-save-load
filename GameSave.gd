652a461aeee79d372997de9b9cc963197d4c5f85
Marc Jones <m4rcj0n35@gmail.com>
2021-06-22 09:36:22 +0100

    Initial commit

---
+++ GameSave.gd
@@ -0,0 +1,10 @@
+extends Node
+
+export var text = ''
+
+func init(input_text):
+	clear_values()
+	text = input_text
+
+func clear_values():
+	text = ''
---
+++ Main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+var save_filepath = "user://test.save"
+var save
+var save_object = preload("res://GameSave.gd")
+
+func _ready():
+	load_state()
+	if save == null:
+		$Text/Label.text += 'null'
+	else:
+		$Text/Label.text += save.text
+	save_state()
+
+func load_state():
+	var save_file = File.new()
+	if save_file.file_exists(save_filepath):
+		save_file.open(save_filepath, File.READ)
+		save = save_file.get_var(true)
+		save_file.close()
+
+func save_state():
+	var new_save = save_object.new()
+	new_save.init("foobar")
+	var save_file = File.new()
+	save_file.open(save_filepath, File.WRITE)
+	save_file.store_var(new_save, true)
+	save_file.close()
